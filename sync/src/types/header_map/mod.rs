d2f33a02b0055bb21713e5753a11b5192d438df5
quake <quake.wang@gmail.com>
2023-04-21 15:08:35 +0900

    chore: replace HeaderMap memory/sled implementation with heed

--- sync/src/types/header_map/mod.rs
+++ sync/src/types/header_map/mod.rs
@@ -1,90 +1,135 @@
 use crate::types::HeaderView;
 use ckb_async_runtime::Handle;
-use ckb_stop_handler::{SignalSender, StopHandler};
-use ckb_types::packed::{self, Byte32};
-use std::path;
-use std::sync::Arc;
-use std::time::Duration;
-use tokio::sync::oneshot;
-use tokio::time::MissedTickBehavior;
-
-mod backend;
-mod backend_sled;
-mod kernel_lru;
-mod memory;
-
-pub(crate) use self::{
-    backend::KeyValueBackend, backend_sled::SledBackend, kernel_lru::HeaderMapKernel,
-    memory::MemoryMap,
-};
+use ckb_types::packed::{Byte32, Byte32Reader};
+use ckb_types::prelude::{Entity, FromSliceShouldBeOk, Reader};
+use heed::{BoxedError, BytesDecode, BytesEncode, Database, Env, EnvOpenOptions, Flags};
+use std::{borrow::Cow, path};
 
 pub struct HeaderMap {
-    inner: Arc<HeaderMapKernel<SledBackend>>,
-    stop: StopHandler<()>,
+    env: Env,
+    db: Database<HeaderMapKey, HeaderMapValue>,
+    _tmpdir: tempfile::TempDir,
 }
 
-impl Drop for HeaderMap {
-    fn drop(&mut self) {
-        self.stop.try_send(());
+pub struct HeaderMapKey;
+
+pub struct HeaderMapValue;
+
+impl BytesEncode<'_> for HeaderMapKey {
+    type EItem = Byte32;
+
+    fn bytes_encode(item: &Self::EItem) -> Result<Cow<[u8]>, BoxedError> {
+        Ok(Cow::from(item.as_slice()))
     }
 }
 
-const INTERVAL: Duration = Duration::from_millis(500);
-// key + total_difficulty + skip_hash
-const ITEM_BYTES_SIZE: usize = packed::HeaderView::TOTAL_SIZE + 32 * 3;
-const WARN_THRESHOLD: usize = ITEM_BYTES_SIZE * 100_000;
+impl<'a> BytesDecode<'a> for HeaderMapKey {
+    type DItem = Byte32;
+
+    fn bytes_decode(bytes: &'a [u8]) -> Result<Self::DItem, BoxedError> {
+        Ok(Byte32Reader::from_slice_should_be_ok(bytes).to_entity())
+    }
+}
+
+impl BytesEncode<'_> for HeaderMapValue {
+    type EItem = HeaderView;
+
+    fn bytes_encode(item: &Self::EItem) -> Result<Cow<[u8]>, BoxedError> {
+        Ok(Cow::from(item.to_vec()))
+    }
+}
+
+impl<'a> BytesDecode<'a> for HeaderMapValue {
+    type DItem = HeaderView;
+
+    fn bytes_decode(bytes: &'a [u8]) -> Result<Self::DItem, BoxedError> {
+        Ok(HeaderView::from_slice_should_be_ok(bytes))
+    }
+}
 
 impl HeaderMap {
-    pub(crate) fn new<P>(tmpdir: Option<P>, memory_limit: usize, async_handle: &Handle) -> Self
+    pub(crate) fn new<P>(tmpdir: Option<P>, _memory_limit: usize, _async_handle: &Handle) -> Self
     where
         P: AsRef<path::Path>,
     {
-        if memory_limit < ITEM_BYTES_SIZE {
-            panic!("The limit setting is too low");
+        let mut builder = tempfile::Builder::new();
+        builder.prefix("ckb-tmp-");
+        let tmpdir = if let Some(ref path) = tmpdir {
+            builder.tempdir_in(path)
+        } else {
+            builder.tempdir()
         }
-        if memory_limit < WARN_THRESHOLD {
-            ckb_logger::warn!(
-                "The low memory limit setting {} will result in inefficient synchronization",
-                memory_limit
-            );
-        }
-        let size_limit = memory_limit / ITEM_BYTES_SIZE;
-        let inner = Arc::new(HeaderMapKernel::new(tmpdir, size_limit));
-        let map = Arc::clone(&inner);
-        let (stop, mut stop_rx) = oneshot::channel::<()>();
+        .expect("failed to create a tempdir to save header map into disk");
 
-        async_handle.spawn(async move {
-            let mut interval = tokio::time::interval(INTERVAL);
-            interval.set_missed_tick_behavior(MissedTickBehavior::Skip);
-            loop {
-                tokio::select! {
-                    _ = interval.tick() => {
-                        map.limit_memory();
-                    }
-                    _ = &mut stop_rx => break,
-                }
-            }
-        });
+        let mut env_builder = EnvOpenOptions::new();
+        // 6GB, around 18,000,000 headers
+        // TODO: make this configurable or increase it dynamically
+        env_builder.map_size(6 * 1024 * 1024 * 1024);
+        env_builder.max_dbs(1);
+        // setup flags for better write performance
+        unsafe {
+            env_builder.flag(Flags::MdbNoSync);
+            env_builder.flag(Flags::MdbNoMetaSync);
+            env_builder.flag(Flags::MdbMapAsync);
+            env_builder.flag(Flags::MdbWriteMap);
+        }
+        let env = env_builder
+            .open(&tmpdir)
+            .expect("failed to open lmdb database to save header map into disk");
+
+        let mut wtxn = env.write_txn().expect("failed to create write transaction");
+        let db: Database<HeaderMapKey, HeaderMapValue> = env
+            .create_database(&mut wtxn, Some("HeaderMap"))
+            .expect("failed to create header map database");
+        wtxn.commit().expect("failed to commit write transaction");
 
         Self {
-            inner,
-            stop: StopHandler::new(SignalSender::Tokio(stop), None, "HeaderMap".to_string()),
+            env,
+            db,
+            _tmpdir: tmpdir,
         }
     }
 
     pub(crate) fn contains_key(&self, hash: &Byte32) -> bool {
-        self.inner.contains_key(hash)
+        let txn = self
+            .env
+            .read_txn()
+            .expect("failed to create read transaction");
+        self.db
+            .get(&txn, hash)
+            .expect("failed to get header from header map")
+            .is_some()
     }
 
     pub(crate) fn get(&self, hash: &Byte32) -> Option<HeaderView> {
-        self.inner.get(hash)
+        let txn = self
+            .env
+            .read_txn()
+            .expect("failed to create read transaction");
+        self.db
+            .get(&txn, hash)
+            .expect("failed to get header from header map")
     }
 
-    pub(crate) fn insert(&self, view: HeaderView) -> Option<()> {
-        self.inner.insert(view)
+    pub(crate) fn insert(&self, view: HeaderView) {
+        let mut txn = self
+            .env
+            .write_txn()
+            .expect("failed to create write transaction");
+        self.db
+            .put(&mut txn, &view.hash(), &view)
+            .expect("failed to insert header into header map");
+        txn.commit().expect("failed to commit write transaction");
     }
 
     pub(crate) fn remove(&self, hash: &Byte32) {
-        self.inner.remove(hash)
+        let mut txn = self
+            .env
+            .write_txn()
+            .expect("failed to create write transaction");
+        self.db
+            .delete(&mut txn, hash)
+            .expect("failed to remove header from header map");
+        txn.commit().expect("failed to commit write transaction");
     }
 }
